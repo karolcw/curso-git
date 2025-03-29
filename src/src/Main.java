217bbddbfdb43518d786ae0a16e76551c014c2ad
Luiz Martins <karoline_cw@hotmail.com>
2025-03-28 22:27:50 -0300

    ajusta mensagem retornada

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,3 +1,3 @@
 public class Main {
-    public static void main(String[] args) { System.out.printf("Hello, Karol and Luiz!"); }
+    public static void main(String[] args) { System.out.printf("Hello, Karol!"); }
 }
